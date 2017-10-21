20c9593d64728327d8c0137532095c1b00f59eac
Lionel Bachelet <lionel@MacBook-Air-de-Lionel.local>
2017-10-21 20:31:14 +0200

    [table] support md css table

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from flask import render_template
 
 from flask_bootstrap import Bootstrap
 from flask_bower import Bower
+from flask_triangle import Triangle
 
 app = Flask(__name__)
 Bootstrap(app)
 Bower(app)
+Triangle(app)
 
 @app.route('/')
 def hello_world():
-    return render_template('base.html')
+    html = render_template('base.html').decode( "utf-8" )
+    return html
--- static/main.js
+++ static/main.js
@@ -2,23 +2,24 @@
 
   'use strict';
 
-  angular.module('mdBurst-api', [])
+  angular.module('mdBurst-api', ['ngSanitize'])
 
   .controller('mdBurst-api-controller', ['$scope', '$log', '$http',
     function($scope, $log, $http) {
-      $log.log("launched");
+      var converter = new showdown.Converter({tables: true});
+      $scope.main_input = "";
+      $scope.main_input_tohmtl = "#hello, markdown!";
 
+      $scope.convert_showdown = function(text) {
+          //text = text.replace(/\r?\n/g, '')
+          $log.log(text);
 
-      $scope.convert_showdown = function() {
-        $log.log("convert_showdown");
-
-        var converter = new showdown.Converter(),
-            text      = '#hello, markdown!',
-            html      = converter.makeHtml(text);
-        $log.log(html);
+          return converter.makeHtml(text);
       };
 
-      $scope.convert_showdown()
+      $scope.$watch('main_input', function() {
+        $scope.main_input_tohmtl = $scope.convert_showdown($scope.main_input);
+      }, true);
   }
   ]);
 
