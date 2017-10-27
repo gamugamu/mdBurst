dd6e44131f7696893dedbb6f6556bda512cbcba9
ABADIE Loïc <loic.abadie@pictime.com>
2017-10-27 16:13:02 +0200

    [TOC] en train d'essayer d'implementer le parse js TOC

--- static/main.js
+++ static/main.js
@@ -4,17 +4,17 @@
 
   angular.module('mdBurst-api', ['ngSanitize'])
 
-
   .controller('mdBurst-api-controller', ['$scope','$log', '$http', '$sce',
     function($scope, $log, $http, $sce) {
-      var converter             = new showdown.Converter({tables: true, ghCompatibleHeaderId: true});
+      var toc                   = require('markdown-toc');
+      var converter             = new showdown.Converter({/*extensions: ['toc'],*/ tables: true, ghCompatibleHeaderId: true, simpleLineBreaks: true, emoji:true});
+      showdown.getOptions();
+
       var defaultOptions        = showdown.getOptions();
       $scope.main_input         = "";
       $scope.main_input_tohmtl  = "";
 
       $scope.convert_showdown = function(text) {
-        //  text = text.replace(/\r?\n/g, '')
-          text = text.replace('# ', '#')
           $scope.html = converter.makeHtml(text);
           return $scope.html;
       };
@@ -23,6 +23,12 @@
           var html = $scope.convert_showdown($scope.main_input)
           $scope.main_input_tohmtl = $sce.trustAsHtml(html);
       }, true);
+
+      $scope.test = function(text) {
+        //  text = text.replace(/\r?\n/g, '')
+          console.log("test")
+
+      };
   }
   ]);
 
