a6682091eb50de05ec6748695f305f077bb7ccb2
ABADIE Loïc <loic.abadie@pictime.com>
2017-10-31 12:51:52 +0100

    [upload], permet de stocker des files sur le site

--- services/FileUpload.py
+++ services/FileUpload.py
@@ -15,43 +15,15 @@ def FileUpload(app):
         return '.' in filename and \
             filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-    @app.route('/upload', methods=['GET', 'POST'])
-    def upload_file():
-        if request.method == 'POST':
-            file = request.files['file']
-
-            if file and allowed_file(file.filename):
-                print "**** ALOWED ****"
-                filename = secure_filename(file.filename)
-                print "**** ALOWED 2 ****"
-                print "PATH ", os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                print "PATH 2", url_for('static', filename='main.js')
-
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                print "dc01-loginrequest"
-                return redirect(url_for('uploaded_file',
-                                    filename=filename))
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form action="" method=post enctype=multipart/form-data>
-        <p><input type=file name=file>
-        <input type=submit value=Upload>
-        </form>
-        '''
-
     @app.route('/upload2', methods=['POST'])
     def upload_file2():
         file = request.files['file']
-        
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-
+            return url_for('uploaded_file', filename=filename)
 
     @app.route('/uploads/<filename>')
     def uploaded_file(filename):
-        return send_from_directory(app.config['UPLOAD_FOLDER'],
-                                   filename)
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
--- static/main.js
+++ static/main.js
@@ -7,17 +7,8 @@
   .controller('mdBurst-api-controller', ['$scope','$log', '$http', '$sce',
     function($scope, $log, $http, $sce) {
       var converter             = new showdown.Converter({tables: true, ghCompatibleHeaderId: true, simpleLineBreaks: true, emoji:true});
-      showdown.getOptions();
-
-      var defaultOptions        = showdown.getOptions();
       $scope.main_input         = "";
       $scope.main_input_tohmtl  = "";
-
-      app.config(['$interpolateProvider', function($interpolateProvider) {
-        $interpolateProvider.startSymbol('{a');
-        $interpolateProvider.endSymbol('a}');
-        console.log("interpolateProvider", file)
-      }]);
 
       $scope.convert_showdown = function(text) {
           text        = parsetest(text)
@@ -34,18 +25,16 @@
       $scope.imageDropped = function(){
           //Get the file
           var file = $scope.uploadedFile;
-          console.log("imageDropped", file)
           var fd = new FormData();
           fd.append('file', file);
-          console.log("fd", fd.get('file'))
-
 
           $http.post("/upload2", fd,{
             transformRequest: angular.identity,
             headers: {'Content-Type': undefined},
            })
           .success(function (data) {
-            console.log("success data");
+            console.log("ssuccess data", data);
+            $scope.main_input += "\n![](" + data + ")"
           })
           .error(function (error) {
             console.log("error");
