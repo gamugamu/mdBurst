2381b216b19d29e1c50cd55a3aa81786d420d549
ABADIE Loïc <loic.abadie@pictime.com>
2017-12-05 17:23:58 +0100

    [ux] dialog box + delete tag

--- static/controllers/post.js
+++ static/controllers/post.js
@@ -4,10 +4,12 @@
 
 // gère les posts
   angular.module('mdBurst-api', ['ngSanitize'])
-  .controller('mdBurst-api-post', ['$scope','$log', '$http', '$sce',
-    function($scope, $log, $http, $sce) {
+  .controller('mdBurst-api-post', ['$scope','$log', '$http', '$sce', '$compile', '$window',
+    function($scope, $log, $http, $sce, $compile, $window) {
       $scope.main_input         = "";
       $scope.main_input_tohmtl  = "";
+      $scope.post_title         = "";
+      $scope.renderTagsHtml     = "";
 
       $('body').height(100);
       // utils
@@ -16,7 +18,7 @@
       };
 
       $scope.category_selected = function(row, column){
-        console.log("uu", $scope.counter_step);
+        console.log("uu", row, column);
         $scope.post_next_step(1);
       };
 
@@ -78,25 +80,60 @@
           }
       }// func
 
+      // tag
+      $scope.add_tag = function(){
+        var text = $sce.trustAsHtml(tag_button($scope.tag_value));
+        var myEl = angular.element(document.querySelector('#renderTags'));
+        myEl.append(tag_button($scope.tag_value, $window.document.getElementsByClassName("tag_deletable").length))
+        $compile(myEl.contents())($scope);
+      };
+
+      function tag_button(tag, idx){
+        return '<span class="mdl-chip mdl-chip--deletable tag_deletable" id=tag_' + parseInt(idx, 10) + '>' +
+               '<span class="mdl-chip__text">' + tag + '</span>' +
+               '<button ng-click="tag_delete('+ idx + ')" ng-value=' + tag + 'type="button" class="mdl-chip__action"><i class="material-icons">cancel</i></button></span>'
+      };
+
+      $scope.tag_delete = function(tag){
+          var myEl = angular.element( document.querySelector( '#tag_'+ tag ) );
+          myEl.remove();
+      }
+
       // call API, save post
-      $scope.postMD =  function(title, payload){
-        $http({
-          method:   'POST',
-          url:      ROOT_DIRECTORY_API_SERVICE + '/dc/post',
-          data: JSON.stringify({
-            "title"   : title,
-            "payload" : $sce.getTrustedHtml(payload)})
-        }).then(function(response) {
-          console.log("post excreated");
-          console.log(response)
-          // redirection homePage. le post doit être en haut de liste.
-          $scope.navigateTo()
-        }); // http
+      $scope.postMD = function(title, payload){
+        console.log("RESULT", $scope.main_input, $scope.post_title);
+
+        if ($scope.main_input == "" && $scope.post_title == "") {
+          var dialog = document.querySelector('dialog');
+
+          if (! dialog.showModal) {
+            dialogPolyfill.registerDialog(dialog);
+          }
+          dialog.showModal();
+          dialog.querySelector('.close').addEventListener('click', function() {
+              dialog.close();
+          });
+        }else{
+          $http({
+            method:   'POST',
+            url:      ROOT_DIRECTORY_API_SERVICE + '/dc/post',
+            data: JSON.stringify({
+              "title"   : title,
+              "payload" : $sce.getTrustedHtml(payload)})
+          }).then(function(response) {
+            // redirection homePage. le post doit être en haut de liste.
+            $scope.navigateTo()
+          }); // http
+        } // if
       }// func
 
       $scope.navigateTo = function(uri = ""){
         window.location.replace(window.location.origin + uri);
       }
+
+      $scope.renderHtml = function (htmlCode) {
+          return $sce.trustAsHtml(htmlCode);
+      };
 
       $scope.post_next_step(-1); // affichage par défault
     }
